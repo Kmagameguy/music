f1f90aad2a5959528f207dfcc2e3a40f610b60af
Kmagameguy <17864518+Kmagameguy@users.noreply.github.com>
2021-03-14 22:48:35 -0400

    Create Script to Generate Templated Post

--- bin/post.rb
+++ bin/post.rb
@@ -1,25 +1,50 @@
-POST_FILTEYPE = "markdown".freeze
+POST_FILETYPE = ".md".freeze
 POST_LAYOUT = "post".freeze
 POST_TIMESTAMP_FORMAT = "%Y-%m-%d".freeze
+
+artist = ARGV[0]
+album = ARGV[1]
 
 def date
   time = Time.now
   time.strftime POST_TIMESTAMP_FORMAT
 end
 
-def extract_spaces(input)
+def hyphenate_spaces(input)
   input = input.gsub(" ", "-")
 end
 
-def process_script_arguments(input)
-  return "Untitled" if input.empty?
-  input.join('-')
+def create_post_title(artist, album)
+  input = date + "-" +
+  hyphenate_spaces(artist) + "-" +
+  hyphenate_spaces(album) + POST_FILETYPE
 end
 
-def create_post_title(input)
-  input = extract_spaces(input)
+def create_new_post(artist, album)
+  filename = create_post_title(artist, album)
+  timestamp = Time.now
+  fileMd = File.new("_posts/#{filename}", "w+")
+  fileMd.write <<EOH
+---
+layout: posts
+artist: #{artist}
+album: #{album}
+date: #{timestamp}
+categories: music
+tracks:
+  -
+genre:
+rating:
+cover:
+videoSlug:
+links:
+  spotify: https://open.spotify.com/album/
+  apple: https://itunes.apple.com/us/album/
+  amazon: https://www.amazon.com/
+---
+EOH
+
+  fileMd.close()
 end
 
-puts date
-postTitle = process_script_arguments(ARGV)
-postTitle = create_post_title(postTitle)
+create_new_post(artist, album)
